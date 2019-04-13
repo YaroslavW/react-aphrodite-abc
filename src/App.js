edffd63fdf30977cb4f5899f905bdb8cc9a60ac9
Yaroslav <doctor355@yahoo.com>
2019-04-13 19:23:46 +0300

    Change React version for Hook using

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { css } from 'aphrodite/no-important';
 import styles from './AppStyles.js';
+
 import './App.css';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         Veniam aliquam quidem quas consectetur assumenda non eveniet natus blanditiis dicta! Nobis fugit officia blanditiis eligendi ipsum! Veniam consequatur, temporibus doloribus laborum sint id obcaecati libero aliquam, eius sit numquam.
         Quod, beatae? Animi, quaerat doloremque. Adipisci iste optio sit accusantium minus tenetur commodi dolore vel id, voluptate saepe sequi ut est doloribus soluta eum blanditiis, quaerat nisi et ea aliquam!</p>
         <div className={css(styles.square, styles.container, styles.center)}></div>
+        
       </div>
     );
   }
